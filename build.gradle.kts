125f729e1095f4ac9d450b46039b2404fc9ce5e5
Ben Kerllenevich <ben@omega24.dev>
2024-03-23 20:03:04 -0400

    Init Gradle Repo

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    java
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+}
