134b1e8fcb5c4e599d44bd608e6c79c35a851faf
YouHaveTrouble <youhavetrouble@youhavetrouble.me>
2024-03-24 23:17:40 +0100

    allow for most basic command handling

--- src/main/java/org/purpurmc/purformance/Main.java
+++ src/main/java/org/purpurmc/purformance/Main.java
@@ -1,29 +1,15 @@
 package org.purpurmc.purformance;
 
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
-import net.kyori.adventure.text.format.TextDecoration;
-import net.minestom.server.MinecraftServer;
-import net.minestom.server.event.GlobalEventHandler;
-import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
-import net.minestom.server.event.server.ServerListPingEvent;
-import net.minestom.server.ping.ResponseData;
-
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
 import java.util.logging.Logger;
 
 public class Main {
 
     public static final Logger logger = Logger.getLogger("Purformance");
+    private static final Server server = new Server();
 
-    public static ServerProperties serverProperties;
-
-    private static final Component kickMessage = Component.text()
-            .append(Component.text("Purformance:").color(NamedTextColor.RED).decorate(TextDecoration.BOLD))
-            .append(Component.newline()).append(Component.newline())
-            .append(Component.text("Player joining has been disabled to increase performance, try again later.").color(NamedTextColor.WHITE).decorate(TextDecoration.ITALIC))
-            .build();
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
 
         long serverStartTime = System.currentTimeMillis();
 
@@ -32,32 +18,22 @@ public class Main {
             System.exit(1);
         }
 
-        serverProperties = new ServerProperties();
+        server.start();
+        server.initialized.get();
 
-        // todo: logging like minecraft console
-
-        MinecraftServer server = MinecraftServer.init();
-        MinecraftServer.setBrandName("Purformance");
-
-        GlobalEventHandler eventHandler = MinecraftServer.getGlobalEventHandler();
-        eventHandler.addListener(ServerListPingEvent.class, event -> {
-            ResponseData data = new ResponseData();
-            data.setDescription(serverProperties.motd);
-            data.setProtocol(serverProperties.fastMode ? 47 : 765);
-            data.setVersion(serverProperties.fastMode ? "1.8.8" : "1.20.4");
-            data.setMaxPlayer(Integer.MAX_VALUE);
-            event.setResponseData(data);
-        });
-
-        eventHandler.addListener(AsyncPlayerConfigurationEvent.class, event -> {
-            event.getPlayer().kick(kickMessage);
-        });
-
-        server.start(serverProperties.ip, serverProperties.port);
         long currentTime = System.currentTimeMillis();
         double elapsedSeconds = (currentTime - serverStartTime) / 1000.0;
         String formattedTime = String.format("%.3fs", elapsedSeconds);
         logger.info("Done (%s)! To get help, just try harder.".formatted(formattedTime));
+
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            logger.info("Console executed command \"%s\"".formatted(input));
+
+            if (input.equalsIgnoreCase("stop")) System.exit(0);
+            // TODO command handling lol
+        }
 
     }
 }
---
+++ src/main/java/org/purpurmc/purformance/Server.java
@@ -0,0 +1,53 @@
+package org.purpurmc.purformance;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.event.GlobalEventHandler;
+import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
+import net.minestom.server.event.server.ServerListPingEvent;
+import net.minestom.server.ping.ResponseData;
+
+import java.util.concurrent.CompletableFuture;
+
+public class Server extends Thread {
+
+    public static ServerProperties serverProperties;
+
+    public final CompletableFuture<Void> initialized = new CompletableFuture<>();
+
+    private static final Component kickMessage = Component.text()
+            .append(Component.text("Purformance:").color(NamedTextColor.RED).decorate(TextDecoration.BOLD))
+            .append(Component.newline()).append(Component.newline())
+            .append(Component.text("Player joining has been disabled to increase performance, try again later.").color(NamedTextColor.WHITE).decorate(TextDecoration.ITALIC))
+            .build();
+
+    @Override
+    public void run() {
+        serverProperties = new ServerProperties();
+
+        // todo: logging like minecraft console
+
+        MinecraftServer server = MinecraftServer.init();
+        MinecraftServer.setBrandName("Purformance");
+
+        GlobalEventHandler eventHandler = MinecraftServer.getGlobalEventHandler();
+        eventHandler.addListener(ServerListPingEvent.class, event -> {
+            ResponseData data = new ResponseData();
+            data.setDescription(serverProperties.motd);
+            data.setProtocol(serverProperties.fastMode ? 47 : 765);
+            data.setVersion(serverProperties.fastMode ? "1.8.8" : "1.20.4");
+            data.setMaxPlayer(Integer.MAX_VALUE);
+            event.setResponseData(data);
+        });
+
+        eventHandler.addListener(AsyncPlayerConfigurationEvent.class, event -> {
+            event.getPlayer().kick(kickMessage);
+        });
+
+        initialized.complete(null);
+        server.start(serverProperties.ip, serverProperties.port);
+    }
+
+}
