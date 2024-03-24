08f66fc4160f5903129ebf21749f3163fd5798f6
YouHaveTrouble <youhavetrouble@youhavetrouble.me>
2024-03-24 22:12:26 +0100

    Temporary logger and eula creation and reading

--- src/main/java/org/purpurmc/purformance/Eula.java
+++ src/main/java/org/purpurmc/purformance/Eula.java
@@ -1,17 +1,53 @@
 package org.purpurmc.purformance;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.StandardCopyOption;
+import java.util.stream.Stream;
+
 public class Eula {
-    private static boolean initialized = false;
+    private static final String EULA_FILE_NAME = "eula.txt";
 
+    /**
+     * Creates eula.txt if it doesn't exist and checksif it contains eula=true
+     * @return true if eula is accepted
+     */
     public static boolean checkEula() {
-        if (!initialized) {
-            initEula();
-        }
-
-        return true; // todo
+        initEula();
+        return eulaAccepted();
     }
 
     private static void initEula() {
-        initialized = true;
+        String jarPath = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getParent();
+        Path eulaFilePath = Paths.get(jarPath, EULA_FILE_NAME);
+        if (!Files.exists(eulaFilePath)) {
+            try (InputStream eulaStream = Main.class.getResourceAsStream("/"+EULA_FILE_NAME)) {
+                if (eulaStream != null) {
+                    Files.copy(eulaStream, eulaFilePath, StandardCopyOption.REPLACE_EXISTING);
+                }
+            } catch (IOException e) {
+                Main.logger.severe("Could not create eula file. Exiting.");
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
     }
+
+    private static boolean eulaAccepted() {
+        String jarPath = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getParent();
+        Path eulaFilePath = Paths.get(jarPath, EULA_FILE_NAME);
+        try (Stream<String> lines = Files.lines(eulaFilePath)) {
+            return lines.anyMatch(line -> line.trim().equalsIgnoreCase("eula=true"));
+        } catch (IOException e) {
+            Main.logger.severe("Could not read eula file. Exiting.");
+            e.printStackTrace();
+            System.exit(1);
+            return false;
+        }
+    }
+
 }
--- src/main/java/org/purpurmc/purformance/Main.java
+++ src/main/java/org/purpurmc/purformance/Main.java
@@ -9,7 +9,12 @@ import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
 import net.minestom.server.event.server.ServerListPingEvent;
 import net.minestom.server.ping.ResponseData;
 
+import java.util.logging.Logger;
+
 public class Main {
+
+    public static final Logger logger = Logger.getLogger("Purformance");
+
     private static final Component kickMessage = Component.text()
             .append(Component.text("Purformance:").color(NamedTextColor.RED).decorate(TextDecoration.BOLD))
             .append(Component.newline()).append(Component.newline())
@@ -19,6 +24,14 @@ public class Main {
     private static Component motd;
 
     public static void main(String[] args) {
+
+        long serverStartTime = System.currentTimeMillis();
+
+        if (!Eula.checkEula()) {
+            logger.severe("You need to accept the eula!");
+            System.exit(1);
+        }
+
         motd = Component.text("Purformance Server");
         // todo: eula & server.properties
         // todo: logging like minecraft console
@@ -41,5 +54,11 @@ public class Main {
         });
 
         server.start("0.0.0.0", 25565);
+        long currentTime = System.currentTimeMillis();
+        double elapsedSeconds = (currentTime - serverStartTime) / 1000.0;
+        String formattedTime = String.format("%.3fs", elapsedSeconds);
+        logger.info("Done (%s)! To get help, just try harder.".formatted(formattedTime));
+
+
     }
 }
