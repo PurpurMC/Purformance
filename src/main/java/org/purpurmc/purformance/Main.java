2ce73224d932c875993c0b4d7decc1bbb909de44
YouHaveTrouble <youhavetrouble@youhavetrouble.me>
2024-03-24 22:50:40 +0100

    server.properties implementation

--- src/main/java/org/purpurmc/purformance/Main.java
+++ src/main/java/org/purpurmc/purformance/Main.java
@@ -15,13 +15,13 @@ public class Main {
 
     public static final Logger logger = Logger.getLogger("Purformance");
 
+    public static ServerProperties serverProperties;
+
     private static final Component kickMessage = Component.text()
             .append(Component.text("Purformance:").color(NamedTextColor.RED).decorate(TextDecoration.BOLD))
             .append(Component.newline()).append(Component.newline())
             .append(Component.text("Player joining has been disabled to increase performance, try again later.").color(NamedTextColor.WHITE).decorate(TextDecoration.ITALIC))
             .build();
-
-    private static Component motd;
 
     public static void main(String[] args) {
 
@@ -32,20 +32,20 @@ public class Main {
             System.exit(1);
         }
 
-        motd = Component.text("Purformance Server");
-        // todo: eula & server.properties
+        serverProperties = new ServerProperties();
+
         // todo: logging like minecraft console
 
         MinecraftServer server = MinecraftServer.init();
+        MinecraftServer.setBrandName("Purformance");
 
         GlobalEventHandler eventHandler = MinecraftServer.getGlobalEventHandler();
         eventHandler.addListener(ServerListPingEvent.class, event -> {
             ResponseData data = new ResponseData();
-
-            data.setPlayersHidden(true);
-            data.setProtocol(765); // 1.20.4 todo: do we want to do like 1.8 or something
-            data.setDescription(motd); // todo
-
+            data.setDescription(serverProperties.motd);
+            data.setProtocol(serverProperties.fastMode ? 47 : 765);
+            data.setVersion(serverProperties.fastMode ? "1.8.8" : "1.20.4");
+            data.setMaxPlayer(Integer.MAX_VALUE);
             event.setResponseData(data);
         });
 
@@ -53,12 +53,11 @@ public class Main {
             event.getPlayer().kick(kickMessage);
         });
 
-        server.start("0.0.0.0", 25565);
+        server.start(serverProperties.ip, serverProperties.port);
         long currentTime = System.currentTimeMillis();
         double elapsedSeconds = (currentTime - serverStartTime) / 1000.0;
         String formattedTime = String.format("%.3fs", elapsedSeconds);
         logger.info("Done (%s)! To get help, just try harder.".formatted(formattedTime));
-
 
     }
 }
---
+++ src/main/java/org/purpurmc/purformance/ServerProperties.java
@@ -0,0 +1,53 @@
+package org.purpurmc.purformance;
+
+import net.kyori.adventure.text.Component;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Properties;
+
+public class ServerProperties {
+
+    private static final String PROPERTIES_FILE_NAME = "server.properties";
+
+    public final String ip;
+    public final Component motd;
+    public final int port;
+    public final boolean fastMode;
+
+    protected ServerProperties() throws RuntimeException {
+        initProperties();
+        String jarPath = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getParent();
+        Path propertiesFilePath = Paths.get(jarPath, PROPERTIES_FILE_NAME);
+        Properties serverProperties = new Properties();
+        try {
+            serverProperties.load(new FileInputStream(String.valueOf(propertiesFilePath)));
+            this.ip = serverProperties.getProperty("ip", "0.0.0.0");
+            this.port = Integer.parseInt(serverProperties.getProperty("port", String.valueOf(25565)));
+            this.motd = Component.text(serverProperties.getProperty("motd", "Purformance server"));
+            this.fastMode = Boolean.parseBoolean(serverProperties.getProperty("fast-mode", "false"));
+        } catch (IOException | NumberFormatException e) {
+            throw new RuntimeException("Failed to read server.properties", e);
+        }
+    }
+
+    private void initProperties() {
+        String jarPath = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getParent();
+        Path propertiesFilePath = Paths.get(jarPath, PROPERTIES_FILE_NAME);
+        if (!Files.exists(propertiesFilePath)) {
+            try (InputStream propertiesStream = Main.class.getResourceAsStream("/"+PROPERTIES_FILE_NAME)) {
+                if (propertiesStream != null) {
+                    Files.copy(propertiesStream, propertiesFilePath, StandardCopyOption.REPLACE_EXISTING);
+                }
+            } catch (IOException e) {
+                Main.logger.severe("Could not create server.properties file. Exiting.");
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
+    }
+
+}
